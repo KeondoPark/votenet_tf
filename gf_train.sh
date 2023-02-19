14743c0110505d3a6a1548aa8fda9298665d4b58
keondopark <gundo0102@gmail.com>
2023-02-19 10:59:21 +0900

    Misc fixes

--- gf_train.sh
+++ gf_train.sh
@@ -28,8 +28,9 @@ CUDA_VISIBLE_DEVICES=2 python train_tf.py --config_path configs/config_rsgf_1way
                        --query_points_generator_loss_coef 0.2 --obj_loss_coef 0.4 --optimizer adamw 
 
 # sunrgbd 2way
-CUDA_VISIBLE_DEVICES=0 python train_tf.py --config_path configs/config_rsgf_2way.json \
-                       --max_epoch 300 --lr-scheduler cosine --cosine_alpha 0.001 \
+CUDA_VISIBLE_DEVICES=1 python train_tf.py --config_path configs/config_rsgf_2way.json \
+                       --max_epoch 300 --lr_decay_steps 150,240,270 --lr_decay_rates 0.1,0.1,0.1 \
+                       --decoder_lr_decay_steps 150,240,270 --decoder_lr_decay_rates 0.1,0.1,0.1 \
                        --size_cls_agnostic --size_delta 0.0625 --heading_delta 0.04 --center_delta 0.1111111111111 \
                        --num_point 20000 --num_decoder_layers 6 \
                        --learning_rate 0.004 --decoder_learning_rate 0.0002 --weight_decay 0.00005 \
