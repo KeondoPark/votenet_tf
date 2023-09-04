4979f02214f1093ce423cbc8006e252347079bbd
jetson <gundo0102@gmail.com>
2023-09-05 00:42:50 +0900

    temp

--- inf_time_check.sh
+++ inf_time_check.sh
@@ -69,12 +69,12 @@ done
 
 # #### scannet 1way CPU + TPU ####    use_multiThr: false, use_edgetpu: true, use_tflite: true
 # #Inf time check for 20 runs
-for i in {1..20}
-do  
-    python3 demo_tf.py --config_path configs/inf_scannet_1way.json --gpu_mem_limit 512 \
-                       --inf_time_file=jetson_inf_time/inf_scannet_1way_cputpu.log --run_point_cpu    
-    sleep 10s 
-done
+# for i in {1..20}
+# do  
+#     python3 demo_tf.py --config_path configs/inf_scannet_1way.json --gpu_mem_limit 512 \
+#                        --inf_time_file=jetson_inf_time/inf_scannet_1way_cputpu.log --run_point_cpu    
+#     sleep 10s 
+# done
 
 #### scannet 2way CPU + TPU ####    use_multiThr: true, use_edgetpu: true, use_tflite: true
 #Inf time check for 20 runs
@@ -156,3 +156,35 @@ done
 #     python3 demo_tf.py --config_path configs/inf_scannet_2way.json --gpu_mem_limit 256 --inf_time_file=jetson_inf_time/inf_scannet_2way_cpugpu.log
 #     sleep 10s 
 # done
+
+# #### SUN RGB-D 2way CPU + GPU ####     use_multiThr: true, use_edgetpu: false, use_tflite: true
+for i in {1..20}
+do  
+    python3 demo_tf.py --config_path configs/inf_2way_fp16.json --gpu_mem_limit 512 \
+                       --inf_time_file=jetson_inf_time/inf_2way_fp16_cpugpu.log
+    sleep 10s 
+done
+
+# #### scannet 2way CPU + GPU ####     use_multiThr: true, use_edgetpu: false, use_tflite: true
+for i in {1..20}
+do  
+    python3 demo_tf.py --config_path configs/inf_scannet_2way_fp16.json --gpu_mem_limit 512 \
+                       --inf_time_file=jetson_inf_time/inf_scannet_2way_fp16_cpugpu.log
+    sleep 10s 
+done
+
+# #### SUN RGB-D 2way CPU + CPU ####     use_multiThr: true, use_edgetpu: false, use_tflite: true
+for i in {1..20}
+do  
+    python3 demo_tf.py --config_path configs/inf_2way_fp16.json --gpu_mem_limit 512 \
+                       --inf_time_file=jetson_inf_time/inf_2way_fp16_2cpu.log
+    sleep 10s 
+done
+
+# #### scannet 2way CPU + CPU ####     use_multiThr: true, use_edgetpu: false, use_tflite: true
+for i in {1..20}
+do  
+    python3 demo_tf.py --config_path configs/inf_scannet_2way_fp16.json --gpu_mem_limit 512 \
+                       --inf_time_file=jetson_inf_time/inf_scannet_2way_fp16_2cpu.log
+    sleep 10s 
+done
